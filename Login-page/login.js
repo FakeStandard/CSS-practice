0d2e8f28daa6cb4df98f914dce3a553480ea1d73
FakeStandard <rileylin.space@gmail.com>
2022-02-08 10:55:44 +0800

    add login page

---
+++ Login-page/login.js
@@ -0,0 +1,9 @@
+  
+$(".txtb input").focus(function(){
+    $(this).addClass("focus");
+})
+$(".txtb input").blur(function(){
+    if($(this).val() == ''){ /* If there is a word, it will not fall */
+        $(this).removeClass("focus");
+    }
+})
